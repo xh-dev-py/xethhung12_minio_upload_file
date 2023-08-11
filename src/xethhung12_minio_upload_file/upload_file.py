38cdb8869e0e3a55f52be7d1e57242563dda8394
xeth hung <adamhung12@gmail.com>
2023-08-11 10:34:13 +0800

    add proxy

--- src/xethhung12_minio_upload_file/upload_file.py
+++ src/xethhung12_minio_upload_file/upload_file.py
@@ -1,8 +1,32 @@
+import os
+
+import urllib3
 from minio import Minio
 
+def create_proxy():
+    proxy_env = os.getenv('http_proxy')
+    if proxy_env != None:
+        print("Using proxy: ", proxy_env)
+    return urllib3.ProxyManager(
+        proxy_env,
+        timeout=urllib3.Timeout.DEFAULT_TIMEOUT,
+        cert_reqs='CERT_REQUIRED',
+        retries=urllib3.Retry(
+            total=5,
+            backoff_factor=0.2,
+            status_forcelist=[500, 502, 503, 504]
+        )
+    ) if proxy_env is not None else None
 
 def upload(url, access_key, secret_key, bucket, file, dest):
+    file = os.path.abspath(file)
+    proxy = create_proxy()
     client = Minio(
+        url,
+        access_key=access_key,
+        secret_key=secret_key,
+        http_client=proxy
+    ) if proxy is not None else Minio(
         url,
         access_key=access_key,
         secret_key=secret_key
@@ -13,5 +37,9 @@ def upload(url, access_key, secret_key, bucket, file, dest):
         object_name=dest,
         file_path=file
     )
-
+    write_etag(file, rs.etag)
     return rs
+
+def write_etag(file_path, etag):
+    with open(f"{file_path}.etag", 'w') as f:
+        f.write(etag)
